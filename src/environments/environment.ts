771d740e0fc53639912cac3c9d210b08a715c224
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-23 21:47:38 -0500

    chore: add environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/sourcer',
+  newsProviderApiKey: 'xyzyxyzyxyz',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/',
+};
