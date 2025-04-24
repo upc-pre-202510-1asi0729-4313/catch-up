ad0461d0b2507398f946142ea049548ff23ca647
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-23 22:10:05 -0500

    fear: add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'catch-up';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+  }
 }
